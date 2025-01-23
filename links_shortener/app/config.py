a50de616ee7f4f5ea812fd3690aa534c4eef14d1
AlexanderIlin2005 <sashailyin2005@gmail.com>
2025-01-23 15:20:18 +0300

    1) Убраны ненужные комментарии 2) Сервис теперь использует общий postgres 3) Убраны ненужные env переменные

--- links_shortener/app/config.py
+++ links_shortener/app/config.py
@@ -4,11 +4,11 @@ import os
 
 class Config:
     load_dotenv()
-    POSTGRES_USER = os.getenv("POSTGRES_SH_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_SH_PASSWORD")
-    POSTGRES_DB = os.getenv("POSTGRES_SH_DB")
-    POSTGRES_HOST = os.getenv("POSTGRES_SH_HOST", "postgres_sh")
-    POSTGRES_PORT = os.getenv("POSTGRES_SH_PORT", 5432)
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_SQL_DB")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
 
     SQLALCHEMY_DATABASE_URI = (f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
                                f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}")
