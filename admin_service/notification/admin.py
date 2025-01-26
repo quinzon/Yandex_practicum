d6f52ef14991554a8030510e4abd482db632f1f0
AlexanderIlin2005 <sashailyin2005@gmail.com>
2025-01-26 14:25:00 +0300

    Улучшение типизации и документации в dev/admin-notification

--- admin_service/notification/admin.py
+++ admin_service/notification/admin.py
@@ -1,3 +1,4 @@
+from typing import Union
 from http import HTTPStatus
 import requests
 
@@ -19,14 +20,44 @@ NOTIFICATION_SERVICE_URL = settings.NOTIFICATION_SERVICE_URL
 
 @admin.register(NotificationTemplate)
 class NotificationTemplateAdmin(PermissionAdmin):
+    """
+    Административный интерфейс для работы с шаблонами уведомлений.
+
+    Этот класс предоставляет административный интерфейс для управления шаблонами уведомлений,
+    включая возможность отправлять уведомления пользователям.
+
+    Атрибуты:
+        resource_name (str): Имя ресурса для шаблона уведомлений.
+        list_display (tuple): Поля, которые будут отображаться в списке объектов шаблонов.
+        search_fields (tuple): Поля для поиска по шаблонам.
+    """
+
     resource_name = 'admin:notification_template'
     list_display = ('title', 'template', 'created_at', 'updated_at', 'send_button')
     search_fields = ('title', 'id')
 
-    def has_send_permission(self, request):
+    def has_send_permission(self, request: Union[HttpRequest, None]) -> bool:
+        """
+        Проверяет, есть ли у пользователя права на отправку уведомлений.
+
+        Параметры:
+            request (HttpRequest): HTTP-запрос, содержащий информацию о текущем пользователе.
+
+        Возвращает:
+            bool: True, если у пользователя есть разрешение на отправку уведомлений, иначе False.
+        """
         return self._check_permission(request, 'send')
 
-    def send_button(self, obj):
+    def send_button(self, obj: NotificationTemplate) -> str:
+        """
+        Генерирует HTML-кнопку для отправки уведомления.
+
+        Параметры:
+            obj (NotificationTemplate): Объект шаблона уведомления.
+
+        Возвращает:
+            str: HTML-код кнопки отправки уведомления.
+        """
         return format_html(
             f'<a class="button" href="{{}}">{_("Send it out")}</a>',
             reverse('admin:send_notification_template', args=[obj.id])
@@ -35,6 +66,14 @@ class NotificationTemplateAdmin(PermissionAdmin):
     send_button.short_description = _('Sending message')
 
     def get_urls(self):
+        """
+        Получает список URL-адресов для административной панели.
+
+        Переопределяет стандартный метод для добавления собственного URL для отправки уведомлений.
+
+        Возвращает:
+            list: Список URL-адресов для административной панели.
+        """
         urls = super().get_urls()
         custom_urls = [
             path('send/<uuid:notification_template_id>/', self.admin_site.admin_view(self.send_notification),
@@ -42,7 +81,20 @@ class NotificationTemplateAdmin(PermissionAdmin):
         ]
         return custom_urls + urls
 
-    def send_notification(self, request, notification_template_id):
+    def send_notification(self, request: Union[HttpRequest, None], notification_template_id: str) -> Union[HttpResponse, None]:
+        """
+        Отправляет уведомление на основе выбранного шаблона.
+
+        Параметры:
+            request (HttpRequest): HTTP-запрос, содержащий данные формы для отправки уведомления.
+            notification_template_id (str): Идентификатор шаблона уведомления.
+
+        Возвращает:
+            HttpResponse: Ответ с результатами операции (редирект после отправки уведомления).
+
+        Исключения:
+            PermissionDenied: Если у пользователя нет прав для отправки уведомлений.
+        """
         if not self.has_send_permission(request):
             raise PermissionDenied("У вас нет прав для отправки уведомлений.")
 
@@ -51,10 +103,10 @@ class NotificationTemplateAdmin(PermissionAdmin):
         if request.method == 'POST':
             form = NotificationRecipientForm(request.POST)
             if form.is_valid():
-                recipient_group = form.cleaned_data['recipients']
-                message_type = form.cleaned_data['message_type']
-                send_time = form.cleaned_data['send_time']
-                priority = form.cleaned_data['priority']
+                recipient_group: str = form.cleaned_data['recipients']
+                message_type: str = form.cleaned_data['message_type']
+                send_time: Union[str, None] = form.cleaned_data['send_time']
+                priority: int = form.cleaned_data['priority']
 
                 response = requests.post(
                     f'{NOTIFICATION_SERVICE_URL}notifications/',
@@ -71,7 +123,7 @@ class NotificationTemplateAdmin(PermissionAdmin):
                         'template_id': str(notification_template_id),
                         'subject': 'admin messaging',
                         'is_delayed': send_time is not None,
-                        'send_time': str(send_time),
+                        'send_time': str(send_time) if send_time else None,
                         'priority': priority,
                         'recipient_group': recipient_group,
                     }
--- admin_service/notification/api/v1/views.py
+++ admin_service/notification/api/v1/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from typing import List, Dict, Union
+from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework.request import Request
+from rest_framework.views import Response as DRFResponse
 
 from notification.models import NotificationTemplate
 from notification.api.v1.serializers import NotificationTemplateModelSerializer, UserDataSerializer
@@ -16,17 +18,47 @@ class NotificationTemplateViewSet(viewsets.ModelViewSet):
 
 @extend_schema(
     parameters=[
-        OpenApiParameter('role', type=str, location=OpenApiParameter.QUERY, description='User role or "all_users"'),
-        OpenApiParameter('page', type=int, location=OpenApiParameter.QUERY, description='Page number'),
-        OpenApiParameter('page_size', type=int, location=OpenApiParameter.QUERY, description='Records per page')
+        OpenApiParameter('role', type=str, location=OpenApiParameter.QUERY, description='Роль пользователя или "all_users"'),
+        OpenApiParameter('page', type=int, location=OpenApiParameter.QUERY, description='Номер страницы'),
+        OpenApiParameter('page_size', type=int, location=OpenApiParameter.QUERY, description='Количество записей на странице')
     ],
     responses={200: UserDataSerializer(many=True)}
 )
 @api_view(['GET'])
-def get_user_data(request):
-    role = request.query_params.get('role')
-    page = request.query_params.get('page', 1)
-    page_size = request.query_params.get('page_size', 50)
+def get_user_data(request: Request) -> DRFResponse:
+    """
+    Получить данные пользователей на основе роли с поддержкой пагинации.
+
+    Эта функция обрабатывает запросы на получение данных о пользователях в зависимости
+    от указанной роли (например, "admin", "user" или "all_users"). Результат возвращается
+    с учётом пагинации.
+
+    Параметры запроса:
+        - role (str): Обязательный параметр. Определяет роль пользователей, которые должны быть возвращены.
+        - page (int, необязательный): Номер страницы. Значение по умолчанию — 1.
+        - page_size (int, необязательный): Количество записей на странице. Значение по умолчанию — 50.
+
+    Параметры:
+        request (Request): HTTP-запрос, содержащий параметры 'role', 'page' и 'page_size'.
+
+    Возвращаемое значение:
+        DRFResponse: JSON-ответ, содержащий данные о пользователях и информацию о пагинации.
+
+        Поля:
+        - `current_page` (int): Текущая страница, на которой находятся результаты.
+        - `page_size` (int): Количество записей на одной странице.
+        - `results` (array): Список пользователей, соответствующих роли. Каждый элемент списка — это объект с полями:
+            - `email` (str): Адрес электронной почты пользователя.
+            - `first_name` (str): Имя пользователя.
+            - `last_name` (str): Фамилия пользователя.
+
+    Исключения:
+        - Возвращает HTTP 400, если параметр 'role' отсутствует.
+        - Возвращает HTTP 400, если параметры 'page' или 'page_size' некорректны.
+    """
+    role: Union[str, None] = request.query_params.get('role')
+    page: Union[str, int] = request.query_params.get('page', 1)
+    page_size: Union[str, int] = request.query_params.get('page_size', 50)
 
     if role is None:
         return Response({'error': "Parameter 'role' is required."}, status=status.HTTP_400_BAD_REQUEST)
@@ -46,9 +78,9 @@ def get_user_data(request):
             status=status.HTTP_400_BAD_REQUEST
         )
 
-    rows = get_users_by_role(role, page, page_size)
+    rows: List[tuple] = get_users_by_role(role, page, page_size)
 
-    result = [
+    result: List[Dict[str, str]] = [
         {
             'email': row[0],
             'first_name': row[1],
@@ -57,7 +89,7 @@ def get_user_data(request):
         for row in rows
     ]
 
-    serializer = UserDataSerializer(data=result, many=True)
+    serializer: UserDataSerializer = UserDataSerializer(data=result, many=True)
     serializer.is_valid(raise_exception=True)
 
     return Response({
