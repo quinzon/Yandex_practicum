98b48267f8acee5a14f8cff71b01cac734aa9de7
AlexanderIlin2005 <sashailyin2005@gmail.com>
2025-01-26 14:59:46 +0300

    Изменен синтаксис Union, уменьшены комментарии в docstring

--- admin_service/notification/admin.py
+++ admin_service/notification/admin.py
@@ -21,43 +21,23 @@ NOTIFICATION_SERVICE_URL = settings.NOTIFICATION_SERVICE_URL
 
 @admin.register(NotificationTemplate)
 class NotificationTemplateAdmin(PermissionAdmin):
-    """
-    Административный интерфейс для работы с шаблонами уведомлений.
-
-    Этот класс предоставляет административный интерфейс для управления шаблонами уведомлений,
-    включая возможность отправлять уведомления пользователям.
-
-    Атрибуты:
-        resource_name (str): Имя ресурса для шаблона уведомлений.
-        list_display (tuple): Поля, которые будут отображаться в списке объектов шаблонов.
-        search_fields (tuple): Поля для поиска по шаблонам.
-    """
-
     resource_name = 'admin:notification_template'
     list_display = ('title', 'template', 'created_at', 'updated_at', 'send_button')
     search_fields = ('title', 'id')
 
     def has_send_permission(self, request: Union[HttpRequest, None]) -> bool:
         """
-        Проверяет, есть ли у пользователя права на отправку уведомлений.
+        Проверяет наличие прав на отправку уведомлений.
 
-        Параметры:
-            request (HttpRequest): HTTP-запрос, содержащий информацию о текущем пользователе.
-
-        Возвращает:
-            bool: True, если у пользователя есть разрешение на отправку уведомлений, иначе False.
+        Возвращает True, если права есть, иначе False.
         """
         return self._check_permission(request, 'send')
 
     def send_button(self, obj: NotificationTemplate) -> str:
         """
-        Генерирует HTML-кнопку для отправки уведомления.
+        Генерирует кнопку для отправки уведомления.
 
-        Параметры:
-            obj (NotificationTemplate): Объект шаблона уведомления.
-
-        Возвращает:
-            str: HTML-код кнопки отправки уведомления.
+        Возвращает HTML-код кнопки.
         """
         return format_html(
             f'<a class="button" href="{{}}">{_("Send it out")}</a>',
@@ -68,12 +48,9 @@ class NotificationTemplateAdmin(PermissionAdmin):
 
     def get_urls(self):
         """
-        Получает список URL-адресов для административной панели.
+        Получает URL-адреса для административной панели.
 
-        Переопределяет стандартный метод для добавления собственного URL для отправки уведомлений.
-
-        Возвращает:
-            list: Список URL-адресов для административной панели.
+        Возвращает список URL-адресов.
         """
         urls = super().get_urls()
         custom_urls = [
@@ -85,17 +62,14 @@ class NotificationTemplateAdmin(PermissionAdmin):
     def send_notification(self, request: Union[HttpRequest, None],
                           notification_template_id: str) -> Union[HttpResponse, None]:
         """
-        Отправляет уведомление на основе выбранного шаблона.
+        Отправляет уведомление на основе шаблона.
 
         Параметры:
-            request (HttpRequest): HTTP-запрос, содержащий данные формы для отправки уведомления.
-            notification_template_id (str): Идентификатор шаблона уведомления.
+            request (HttpRequest): Данные формы отправки уведомления.
+            notification_template_id (str): Идентификатор шаблона.
 
         Возвращает:
-            HttpResponse: Ответ с результатами операции (редирект после отправки уведомления).
-
-        Исключения:
-            PermissionDenied: Если у пользователя нет прав для отправки уведомлений.
+            HttpResponse: Редирект после отправки уведомления.
         """
         if not self.has_send_permission(request):
             raise PermissionDenied("У вас нет прав для отправки уведомлений.")
@@ -105,10 +79,10 @@ class NotificationTemplateAdmin(PermissionAdmin):
         if request.method == 'POST':
             form = NotificationRecipientForm(request.POST)
             if form.is_valid():
-                recipient_group: str = form.cleaned_data['recipients']
-                message_type: str = form.cleaned_data['message_type']
-                send_time: Union[str, None] = form.cleaned_data['send_time']
-                priority: int = form.cleaned_data['priority']
+                recipient_group = form.cleaned_data['recipients']
+                message_type = form.cleaned_data['message_type']
+                send_time = form.cleaned_data['send_time']
+                priority = form.cleaned_data['priority']
 
                 response = requests.post(
                     f'{NOTIFICATION_SERVICE_URL}notifications/',
